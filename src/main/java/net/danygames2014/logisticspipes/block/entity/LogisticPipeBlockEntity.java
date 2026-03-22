41c5b274d1268994ec2ccb933236b8428ca75b60
Ralf2oo2 <ralf2oo2@hotmail.com>
2026-03-22 17:47:49 +0000

    add item routing queue

--- src/main/java/net/danygames2014/logisticspipes/block/entity/LogisticPipeBlockEntity.java
+++ src/main/java/net/danygames2014/logisticspipes/block/entity/LogisticPipeBlockEntity.java
@@ -5,6 +5,8 @@ import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import net.danygames2014.buildcraft.block.PipeBlock;
 import net.danygames2014.buildcraft.block.entity.pipe.PipeBlockEntity;
 import net.danygames2014.buildcraft.block.entity.pipe.PipeConnectionType;
+import net.danygames2014.buildcraft.block.entity.pipe.PipeTransporter;
+import net.danygames2014.buildcraft.block.entity.pipe.transporter.ItemPipeTransporter;
 import net.danygames2014.logisticspipes.block.pipe.ItemSendMode;
 import net.danygames2014.logisticspipes.config.Config;
 import net.danygames2014.logisticspipes.gui.hud.TestHud;
@@ -15,9 +17,11 @@ import net.danygames2014.logisticspipes.interfaces.RoutedItem;
 import net.danygames2014.logisticspipes.routing.RouteDestination;
 import net.danygames2014.logisticspipes.routing.Router;
 import net.danygames2014.logisticspipes.util.RoutingUtil;
+import net.danygames2014.logisticspipes.util.tuple.Pair;
 import net.danygames2014.logisticspipes.util.tuple.Pair3;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
 import net.modificationstation.stationapi.api.util.math.Direction;
 
@@ -31,7 +35,15 @@ public abstract class LogisticPipeBlockEntity extends PipeBlockEntity implements
 
     private boolean enabled = true;
 
-    protected final LinkedList<Pair3<RoutedItem, Direction, ItemSendMode>> _sendQueue = new LinkedList<>();
+    public int statSessionSent;
+    public int statSessionReceived;
+    public int statSessionRelayed;
+
+    public long statLifetimeSent;
+    public long statLifetimeReceived;
+    public long statLifetimeRelayed;
+
+    protected final LinkedList<Pair3<RoutedItem, Direction, ItemSendMode>> sendQueue = new LinkedList<>();
 
     // Routing
     // Routing Table: Destination Router ID -> Next Hop Router ID
@@ -74,6 +86,35 @@ public abstract class LogisticPipeBlockEntity extends PipeBlockEntity implements
             updateNeighbors = false;
         }
 
+        if(!sendQueue.isEmpty()){
+            if(getItemSendMode() == ItemSendMode.Normal) {
+                Pair<RoutedItem, Direction> itemToSend = sendQueue.getFirst();
+                sendRoutedItem(itemToSend.getValue1(), itemToSend.getValue2());
+                sendQueue.removeFirst();
+                for(int i = 0; i < 16 && !sendQueue.isEmpty() && sendQueue.getFirst().getValue3() == ItemSendMode.Fast; i++){
+                    if(!sendQueue.isEmpty()){
+                        itemToSend = sendQueue.getFirst();
+                        sendRoutedItem(itemToSend.getValue1(), itemToSend.getValue2());
+                        sendQueue.removeFirst();
+                    }
+                }
+                sendQueueChanged();
+            } else if (getItemSendMode() == ItemSendMode.Fast) {
+                for(int i = 0; i < 16; i++) {
+                    if(!sendQueue.isEmpty()) {
+                        Pair<RoutedItem, Direction> itemToSend = sendQueue.getFirst();
+                        sendRoutedItem(itemToSend.getValue1(), itemToSend.getValue2());
+                        sendQueue.removeFirst();
+                    }
+                }
+                sendQueueChanged();
+            } else if(getItemSendMode() == null) {
+                throw new UnsupportedOperationException("getItemSendMode() can't return null. " + this.getClass().getName());
+            } else {
+                throw new UnsupportedOperationException("getItemSendMode() returned unhandled value. " + getItemSendMode().name() + " in " + this.getClass().getName());
+            }
+        }
+
         if (getLogisticsModule() == null) {
             return;
         }
@@ -81,7 +122,6 @@ public abstract class LogisticPipeBlockEntity extends PipeBlockEntity implements
         if (!isEnabled()) {
             return;
         }
-
         getLogisticsModule().tick();
     }
 
@@ -96,17 +136,24 @@ public abstract class LogisticPipeBlockEntity extends PipeBlockEntity implements
     public abstract ItemSendMode getItemSendMode();
 
     public void queueRoutedItem(RoutedItem routedItem, Direction from) {
-        _sendQueue.addLast(new Pair3<>(routedItem, from, ItemSendMode.Normal));
+        sendQueue.addLast(new Pair3<>(routedItem, from, ItemSendMode.Normal));
         sendQueueChanged();
     }
 
     public void queueRoutedItem(RoutedItem routedItem, Direction from, ItemSendMode mode) {
-        _sendQueue.addLast(new Pair3<>(routedItem, from, mode));
+        sendQueue.addLast(new Pair3<>(routedItem, from, mode));
         sendQueueChanged();
     }
 
-    protected void sendQueueChanged() {
+    private void sendRoutedItem(RoutedItem routedItem, Direction from){
+        if(transporter instanceof ItemPipeTransporter itemTransporter){
+            itemTransporter.receiveTravellingItem(routedItem.getTravellingItemEntity(), from.getOpposite());
+            statLifetimeSent++;
+            statSessionSent++;
+        }
     }
+
+    protected void sendQueueChanged() {}
 
     @Override
     public HUDRenderer getRenderer() {
@@ -312,5 +359,31 @@ public abstract class LogisticPipeBlockEntity extends PipeBlockEntity implements
 
     private record SearchNode(int x, int y, int z, int metric, Direction firstDir) {
 
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
+
+        statLifetimeSent = nbt.getLong("statLifetimeSent");
+        statLifetimeReceived = nbt.getLong("statLifetimeReceived");
+        statLifetimeRelayed = nbt.getLong("statLifetimeReplayed");
+
+        if(getLogisticsModule() != null) {
+            getLogisticsModule().readNbt(nbt, "");
+        }
+    }
+
+    @Override
+    public void writeNbt(NbtCompound nbt) {
+        super.writeNbt(nbt);
+
+        nbt.putLong("statLifetimeSent", statLifetimeSent);
+        nbt.putLong("statLifetimeReceived", statLifetimeReceived);
+        nbt.putLong("statLifetimeReplayed", statLifetimeRelayed);
+
+        if(getLogisticsModule() != null) {
+            getLogisticsModule().writeNbt(nbt, "");
+        }
     }
 }
