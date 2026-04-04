826defdabc990681d10c65c85b61af2cbf04c8e1
Ralf2oo2 <ralf2oo2@hotmail.com>
2026-04-04 19:08:16 +0000

    last commit before pain

---
+++ src/main/java/net/danygames2014/logisticspipes/util/ItemIdentifier.java
@@ -0,0 +1,87 @@
+package net.danygames2014.logisticspipes.util;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.resource.language.TranslationStorage;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+
+import java.util.LinkedList;
+
+public class ItemIdentifier {
+    private final static LinkedList<ItemIdentifier> itemIdentifierCacheServer = new LinkedList<>();
+    private final static LinkedList<ItemIdentifier> itemIdentifierCacheClient = new LinkedList<>();
+
+    private ItemIdentifier(Item item, int itemDamage, NbtCompound nbt){
+        this.item = item;
+        this.itemDamage = itemDamage;
+        this.nbt = nbt;
+    }
+
+    public final Item item;
+    public final int itemDamage;
+    public final NbtCompound nbt;
+
+    public static boolean allowNullsForTesting;
+
+    public static ItemIdentifier get(Item item, int itemDamage, NbtCompound nbt){
+        for(ItemIdentifier itemIdentifier : FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT ? itemIdentifierCacheClient : itemIdentifierCacheServer){
+            if(itemIdentifier.item == item && itemIdentifier.itemDamage == itemDamage && nbtequal(itemIdentifier.nbt, nbt)){
+                return itemIdentifier;
+            }
+        }
+        ItemIdentifier unknownItem = new ItemIdentifier(item, itemDamage, nbt);
+        (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT ? itemIdentifierCacheClient : itemIdentifierCacheServer).add(unknownItem);
+        return unknownItem;
+    }
+
+    public static ItemIdentifier get(ItemStack stack) {
+        if(stack == null && allowNullsForTesting){
+            return null;
+        }
+        int itemDamage = 0;
+        if(!stack.getItem().isDamageable()){
+            itemDamage = stack.getDamage();
+        }
+        return get(stack.getItem(), itemDamage, stack.getStationNbt());
+    }
+
+    private static boolean nbtequal(NbtCompound nbt1, NbtCompound nbt2) {
+        if(nbt1 == null && nbt2 == null) {
+            return true;
+        }
+        if(nbt1 == null) {
+            return false;
+        }
+        if(nbt2 == null) {
+            return false;
+        }
+        return nbt1.equals(nbt2);
+    }
+
+    private String getName(ItemStack stack) {
+
+        return TranslationStorage.getInstance().get(stack.getTranslationKey() + ".name");
+
+    }
+
+    public String getFriendlyName() {
+        return getName(this.makeNormalStack(1));
+    }
+
+    public ItemIdentifierStack makeStack(int stackSize){
+        return new ItemIdentifierStack(this, stackSize);
+    }
+
+    public ItemStack makeNormalStack(int stackSize){
+        ItemStack stack = new ItemStack(this.item, stackSize, this.itemDamage);
+        stack.getStationNbt().values().addAll(nbt.values());
+        return stack;
+    }
+
+    @Override
+    public String toString() {
+        return getFriendlyName();
+    }
+}
---
+++ src/main/java/net/danygames2014/logisticspipes/util/ItemIdentifierStack.java
@@ -0,0 +1,92 @@
+package net.danygames2014.logisticspipes.util;
+
+import net.danygames2014.logisticspipes.block.pipe.ItemSendMode;
+import net.danygames2014.logisticspipes.interfaces.RoutedItem;
+import net.danygames2014.logisticspipes.util.tuple.Pair3;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.modificationstation.stationapi.api.util.math.Direction;
+
+import java.util.LinkedList;
+
+public class ItemIdentifierStack {
+    private final ItemIdentifier item;
+    public int stackSize;
+
+    public static ItemIdentifierStack getFromStack(ItemStack stack) {
+        return new ItemIdentifierStack(ItemIdentifier.get(stack), stack.count);
+    }
+
+    public ItemIdentifierStack(ItemIdentifier item, int stackSize){
+        this.item = item;
+        this.stackSize = stackSize;
+    }
+
+    public ItemIdentifier getItem(){
+        return item;
+    }
+
+    public ItemStack makeNormalStack(){
+        ItemStack stack = new ItemStack(item.item, this.stackSize, item.itemDamage);
+        stack.getStationNbt().values().addAll(item.nbt.values());
+        return stack;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if(object instanceof ItemIdentifierStack stack) {
+            return stack.item.equals(this.item) && stack.stackSize == this.stackSize;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return stackSize + "x" + item.toString();
+    }
+
+    @Override
+    public ItemIdentifierStack clone() {
+        return new ItemIdentifierStack(item, stackSize);
+    }
+
+    public static LinkedList<ItemIdentifierStack> getListFromInventory(Inventory inv) {
+        return getListFromInventory(inv, false);
+    }
+
+    public static LinkedList<ItemIdentifierStack> getListFromInventory(Inventory inv, boolean removeNull) {
+        LinkedList<ItemIdentifierStack> list = new LinkedList<>();
+        for(int i=0;i<inv.size();i++) {
+            if(inv.getStack(i) == null) {
+                if(!removeNull) {
+                    list.add(null);
+                }
+            } else {
+                list.add(ItemIdentifierStack.getFromStack(inv.getStack(i)));
+            }
+        }
+        return list;
+    }
+
+    public static LinkedList<ItemIdentifierStack> getListSendQueue(LinkedList<Pair3<RoutedItem, Direction, ItemSendMode>> _sendQueue) {
+        LinkedList<ItemIdentifierStack> list = new LinkedList<ItemIdentifierStack>();
+        for(Pair3<RoutedItem, Direction, ItemSendMode> part:_sendQueue) {
+            if(part == null) {
+                list.add(null);
+            } else {
+                boolean added = false;
+                for(ItemIdentifierStack stack:list) {
+                    if(stack.getItem().equals(ItemIdentifierStack.getFromStack(part.getValue1().getItemStack()).getItem())) {
+                        stack.stackSize += part.getValue1().getItemStack().count;
+                        added = true;
+                        break;
+                    }
+                }
+                if(!added) {
+                    list.add(ItemIdentifierStack.getFromStack(part.getValue1().getItemStack()));
+                }
+            }
+        }
+        return list;
+    }
+}
