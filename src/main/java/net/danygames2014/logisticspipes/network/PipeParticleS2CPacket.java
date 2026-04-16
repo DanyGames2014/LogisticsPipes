35771d7c8c93267c483ec6673081f534b1f7258e
Daniel Fildán <fildand@centrum.cz>
2026-04-16 11:45:08 +0200

    Fix Pipe Particle Packet crashing on server

--- src/main/java/net/danygames2014/logisticspipes/network/PipeParticleS2CPacket.java
+++ src/main/java/net/danygames2014/logisticspipes/network/PipeParticleS2CPacket.java
@@ -2,6 +2,8 @@ package net.danygames2014.logisticspipes.network;
 
 import net.danygames2014.logisticspipes.client.particle.SparkleParticle;
 import net.danygames2014.logisticspipes.util.ParticleColor;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.NetworkHandler;
@@ -10,6 +12,7 @@ import net.minecraft.util.math.BlockPos;
 import net.modificationstation.stationapi.api.entity.player.PlayerHelper;
 import net.modificationstation.stationapi.api.network.packet.ManagedPacket;
 import net.modificationstation.stationapi.api.network.packet.PacketType;
+import net.modificationstation.stationapi.api.util.SideUtil;
 import net.modificationstation.stationapi.api.util.math.StationBlockPos;
 import org.jetbrains.annotations.NotNull;
 
@@ -26,7 +29,8 @@ public class PipeParticleS2CPacket extends Packet implements ManagedPacket<PipeP
     private int[] queuedParticles = new int[ParticleColor.values().length];
     private BlockPos pos;
 
-    public PipeParticleS2CPacket(){}
+    public PipeParticleS2CPacket() {
+    }
 
     public PipeParticleS2CPacket(int[] queuedParticles, int x, int y, int z) {
         this.queuedParticles = queuedParticles;
@@ -36,11 +40,11 @@ public class PipeParticleS2CPacket extends Packet implements ManagedPacket<PipeP
     @Override
     public void read(DataInputStream stream) {
         try {
-            for(int i = 0; i < queuedParticles.length; i++) {
+            for (int i = 0; i < queuedParticles.length; i++) {
                 queuedParticles[i] = stream.readInt();
             }
             pos = StationBlockPos.fromLong(stream.readLong());
-        } catch (IOException ignored){
+        } catch (IOException ignored) {
 
         }
     }
@@ -48,23 +52,31 @@ public class PipeParticleS2CPacket extends Packet implements ManagedPacket<PipeP
     @Override
     public void write(DataOutputStream stream) {
         try {
-            for(int i = 0; i < queuedParticles.length; i++) {
+            for (int i = 0; i < queuedParticles.length; i++) {
                 stream.writeInt(queuedParticles[i]);
             }
             stream.writeLong(pos.asLong());
-        } catch (IOException ignored){
+        } catch (IOException ignored) {
 
         }
     }
 
     @Override
     public void apply(NetworkHandler networkHandler) {
+        SideUtil.run(() -> {
+            applyClient(networkHandler);
+        }, () -> {
+        });
+    }
+
+    @Environment(EnvType.CLIENT)
+    public void applyClient(NetworkHandler networkHandler) {
         PlayerEntity player = PlayerHelper.getPlayerFromPacketHandler(networkHandler);
-        if(player != null && player.world != null){
-            for(int i = 0; i < queuedParticles.length; i++) {
+        if (player != null && player.world != null) {
+            for (int i = 0; i < queuedParticles.length; i++) {
                 int amount = queuedParticles[i];
                 ParticleColor color = ParticleColor.values()[i];
-                if(amount > 0) {
+                if (amount > 0) {
                     float boundry = 0.4F;
                     int pipeWidth = 3;
 
@@ -72,7 +84,7 @@ public class PipeParticleS2CPacket extends Packet implements ManagedPacket<PipeP
                     float length = boundry + random.nextInt(pipeWidth) / 10.0F;
                     float height = random.nextInt(7) / 10.0F + 0.2F;
 
-                    float scalemult = 1f + (float)Math.log10(amount);
+                    float scalemult = 1f + (float) Math.log10(amount);
 
                     Minecraft.INSTANCE.particleManager.addParticle(new SparkleParticle(player.world, pos.getX() + length, pos.getY() + height, pos.getZ() + width, scalemult, color.getR(), color.getG(), color.getB(), 6 + random.nextInt(3)));
                 }
