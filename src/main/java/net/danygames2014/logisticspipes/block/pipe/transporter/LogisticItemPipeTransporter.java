f891dcf5182c9fb0875b2a42a3100eb3c070db46
Daniel Fildán <fildand@centrum.cz>
2026-05-31 16:43:28 +0200

    Fix items being sent to the far away lands with no return ticket

--- src/main/java/net/danygames2014/logisticspipes/block/pipe/transporter/LogisticItemPipeTransporter.java
+++ src/main/java/net/danygames2014/logisticspipes/block/pipe/transporter/LogisticItemPipeTransporter.java
@@ -4,23 +4,23 @@ import net.danygames2014.buildcraft.block.entity.pipe.PipeBlockEntity;
 import net.danygames2014.buildcraft.block.entity.pipe.transporter.ItemPipeTransporter;
 import net.danygames2014.buildcraft.entity.TravellingItemEntity;
 import net.danygames2014.logisticspipes.block.entity.RequestTableLogisticPipeBlockEntity;
-import net.minecraft.block.entity.BlockEntity;
 
 public class LogisticItemPipeTransporter extends ItemPipeTransporter {
-    private BlockEntity blockEntity;
     public LogisticItemPipeTransporter(PipeBlockEntity blockEntity) {
         super(blockEntity);
-        this.blockEntity = blockEntity;
     }
 
     @Override
     public void dropItem(TravellingItemEntity item) {
-        if(blockEntity instanceof RequestTableLogisticPipeBlockEntity) {
-            if(item.stack != null && item.stack.count > 0) {
+        if (blockEntity instanceof RequestTableLogisticPipeBlockEntity) {
+            if (item.stack != null && item.stack.count > 0) {
                 super.dropItem(item);
             } else {
                 item.markDead();
             }
+            return;
         }
+        
+        super.dropItem(item);
     }
 }
